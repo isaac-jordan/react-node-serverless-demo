c930694ad979396f82ec7285c0e78e6f79b13746
Isaac Jordan <isaacjordan95@gmail.com>
2020-01-08 21:38:53 +0000

    Add basic React app

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-1",
+    BUCKET: "react-node-serverless-demo-notes-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://zlut8kw88j.execute-api.eu-west-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_V937KhELV",
+    APP_CLIENT_ID: "51csr4pju4584kmad7rscn9l5o",
+    IDENTITY_POOL_ID: "eu-west-1:fdcb7aa6-693a-4983-a14e-afbfc6481753"
+  }
+};
